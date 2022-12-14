eef573dd12492192db2d57e068e4ff777fcbea84
LucianoBrumer <lucianobrumer5@gmail.com>
2022-12-14 02:04:10 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys, os, argparse
+
+path = os.getcwd()
+
+parser = argparse.ArgumentParser(
+    prog='GitPush',
+    description='A fast method to push'
+)
+parser.add_argument('--commit', '-c', help='Commit (default: '')', default='')
+parser.add_argument('--branch', '-b', help='Branch name (default: main)', default='main')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    command = f'cd "{path}" && git add . && git commit -m "{args.commit}" && git push origin {args.branch}'
+
+    print(command)
+    os.system(command)

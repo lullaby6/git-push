6f3b48a868e6b18f1b812efde7f12bf6a2404261
LucianoBrumer <lucianobrumer5@gmail.com>
2022-12-14 02:19:05 -0300

    commit with gitpush!

--- gitpush.py
+++ gitpush.py
@@ -7,12 +7,17 @@ parser = argparse.ArgumentParser(
     description='A fast method to push'
 )
 
-parser.add_argument('--commit', '-c', help='Commit (default: '')', default='')
+parser.add_argument('--commit', '-c', help='Commit (default: "commit with gitpush!")', default='commit with gitpush!')
 parser.add_argument('--branch', '-b', help='Branch name (default: main)', default='main')
 
 if __name__ == '__main__':
-    args = parser.parse_args()
+    try:
+        args = parser.parse_args()
 
-    command = f'cd "{path}" && git add . && git commit -m "{args.commit}" && git push origin {args.branch}'
+        command = f'cd "{path}" && git add . && git commit -m "{args.commit}" && git push origin {args.branch}'
 
-    os.system(command)
+        print(command)
+        res = os.popen(command).read()
+        print(res)
+    except Exception as e:
+        print(e)
